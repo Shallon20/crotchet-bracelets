08645f6f509967036df1c7464f87b1c34b82c3e2
Shallon <shallonmaria2001@gmail.com>
2024-12-02 11:57:43 +0200

    Add to cart

--- cart/views.py
+++ cart/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from my_app.models import Product
+from django.http import JsonResponse
+
+
 
 # Create your views here.
 def cart_summary(request):
     return render(request, 'cart_summary.html', {})
 def cart_add(request):
-    pass
+    # get the cart
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        # get stuff
+        product_id = int(request.POST.get('product_id'))
+        # lookup product in database
+        product = get_object_or_404(Product, id=product_id)
+        #save to session
+        cart.add(product=product)
+        #return response
+        response = JsonResponse({'Product Name': 'product.name'})
+        return response
 def cart_delete(request):
     pass
 def cart_update(request):
