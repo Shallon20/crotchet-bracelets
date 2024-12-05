1966d228b3362435ea60962679109d76d6664eaa
Shallon <shallonmaria2001@gmail.com>
2024-12-05 15:07:08 +0200

    Fixed search

--- my_app/urls.py
+++ my_app/urls.py
@@ -4,11 +4,10 @@ from . import views
 
 urlpatterns = [
                   path('', views.home, name='home'),
-                  path('about', views.about, name='about'),
                   path('product_list', views.product_list, name='products'),
                   path('category/<str:foo>', views.category, name='category'),
                   path('category_summary', views.category_summary, name='category_summary'),
-                  # path('product_list/search', views.search_product, name='search_product'),
+                  path('search', views.search_product, name='search_product'),
                   path('contact_us', views.contact_us, name='contact_us'),
                   path('thank_you', views.thank_you, name='thank_you'),
                   path('login', views.login_user, name='login'),
--- my_app/views.py
+++ my_app/views.py
@@ -17,10 +17,6 @@ def home(request):
     return render(request, 'home.html', {"products": products, "latest_products": latest_products})
 
 
-def about(request):
-    return render(request, 'about.html')
-
-
 def product_list(request):
     category = request.GET.get('category')
     if category:
@@ -29,11 +25,6 @@ def product_list(request):
         products = Product.objects.all().order_by('-created_at')
     categories = Category.objects.all()
     return render(request, 'products.html', {"products": products, "categories": categories})
-
-
-def product_detail(request, product_id):
-    products = get_object_or_404(Product, id=product_id)
-    return render(request, 'product_detail.html', {'products': products})
 
 
 def contact_us(request):
@@ -136,123 +127,13 @@ def thank_you(request):
     return render(request, 'thank_you.html')
 
 
-@login_required
-def update_product(request, product_id):
-    return None
-
-
-# @login_required
-# def cart(request):
-#     cart_items = CartItem.objects.filter(user=request.user)
-#     total_price = sum(item.total_price() for item in cart_items)
-#     return render(request, 'cart.html',{
-#         'cart_items': cart_items,
-#         'total_price': total_price
-#     })
-# @login_required
-# def add_to_cart(request, product_id):
-#     product = get_object_or_404(Product, id=product_id)
-#     cart_item, created = CartItem.objects.get_or_create(
-#         user=request.user,
-#         product=product,
-#         defaults={'quantity': 1},
-#     )
-#     if not created:
-#         cart_item.quantity += 1
-#         cart_item.save()
-#     messages.success(request, f"{product.name} added to your cart!")
-#     return redirect('cart')
-# @login_required()
-# def place_order(request):
-#     cart_items = CartItem.objects.filter(user=request.user)
-#     if not cart_items:
-#         messages.error(request, "Your cart is empty! Add items before placing an order.")
-#         return redirect('cart')
-#     total_amount = sum(item.total_price() for item in cart_items)
-#
-#     order = CartOrder.objects.create(
-#         user=request.user,
-#         total_amount=total_amount,
-#         shipping_address=request.POST.get('shipping_address', 'Default Address'),
-#
-#     )
-#     for item in cart_items:
-#         item.delete()
-#
-#     messages.success(request, "Your order has been placed successfully!.")
-#     return redirect('home')
-# @login_required()
-# def update_cart_quantity(request, cart_item_id, action):
-#     cart_item = get_object_or_404(CartItem, id=cart_item_id, user=request.user)
-#
-#     if action == 'increase':
-#         cart_item.quantity += 1
-#     elif action == 'decrease' and cart_item.quantity > 1:
-#         cart_item.quantity -= 1
-#     else:
-#         cart_item.delete()
-#         return JsonResponse({
-#         'quantity': 0,
-#         'total_price':  0,
-#         'cart_total': sum(item.total_price() for item in cart_item.objects.filter(user=request.user)),
-#         })
-#     cart_item.save()
-#     return JsonResponse({
-#         'quantity': cart_item.quantity,
-#         'total_price': cart_item.total_price(),
-#         'cart_total': sum(item.total_price() for item in CartItem.objects.filter(user=request.user)),
-#     })
-
-# @login_required()
-# def remove_cart_item(request, cart_item_id):
-#     cart_item = get_object_or_404(CartItem, id=cart_item_id, user=request.user)
-#     cart_item.delete()
-#     messages.success(request, "Your cart item has been removed successfully!")
-#     return JsonResponse({'status': 'success'})
-
-
-# @login_required
-# @permission_required("my_app.delete_product", raise_exception=True)
-# def delete_product(request, product_id):
-#     product = Product.objects.get(id=product_id)
-#     product.delete()
-#     messages.info(request, f"Customer {product.name} was deleted!!")
-#     return redirect('products')
-#
-# @login_required
-# @permission_required("my_app.add_product", raise_exception=True)
-# def add_product(request):
-#     if request.method == "POST":
-#         form = ProductForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             messages.success(request, f"Product {form.cleaned_data['name']} was added!")
-#             return redirect('products')
-#     else:
-#         form = ProductForm()
-#     return render(request, 'product_add_form.html', {"form": form})
-#
-# @login_required
-# @permission_required("my_app.change_product", raise_exception=True)
-# def update_product(request, product_id):
-#     product = get_object_or_404(Product, id=product_id)
-#     if request.method == "POST":
-#         form = ProductForm(request.POST, request.FILES, instance=product)
-#         if form.is_valid():
-#             form.save()
-#             messages.success(request, f"Product {form.cleaned_data['name']} was updated!")
-#             return redirect('products')
-#     else:
-#         form = ProductForm(instance=product)
-#     return render(request, 'product_update_form.html', {"form": form})
-
-#
 # @login_required
 # @permission_required("my_app.view_product", raise_exception=True)
 def search_product(request):
     if request.method == "POST":
-        searched = request.POST['searched']
-        products = Product.objects.filter(name__contains=searched)
-        return render(request, "search.html", {'searched': searched, 'products': products})
-    else:
-        return render(request, "search.html", {})
+        searched = request.POST.get('searched', '').strip()
+        if searched:
+            products = Product.objects.filter(name__icontains=searched)
+            return render(request, "search.html", {'searched': searched, 'products': products})
+        return render(request, "search.html", {'error': 'Please enter a search term.'})
+    return redirect('home')
