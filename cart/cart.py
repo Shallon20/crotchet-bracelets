e651bc8e500c0d8c4791525a17fede098d6d2f04
Shallon <shallonmaria2001@gmail.com>
2024-12-05 20:47:56 +0200

    cart persistence on logout

--- cart/cart.py
+++ cart/cart.py
@@ -1,11 +1,13 @@
 from venv import logger
 
-from my_app.models import Product
+from my_app.models import Product, Profile
 
 
 class Cart:
     def __init__(self, request):
         self.session = request.session
+        # get request
+        self.request = request
 
         # Get the current session key if it exists
         cart = self.session.get('session_key')
@@ -28,6 +30,16 @@ class Cart:
             self.cart[product_id] = int(product_qty)
 
         self.session.modified = True
+        # deal with logged in user
+        if self.request.user.is_authenticated:
+            # get current user
+            current_user = Profile.objects.filter(user__id=self.request.user.id)
+             # convert single quotes to double quotes
+            carty = str(self.cart)
+            carty = carty.replace("\'", "\"")
+            #save carty to profile model
+            current_user.update(old_cart=str(carty))
+
 
     def __len__(self):
         return len(self.cart)
